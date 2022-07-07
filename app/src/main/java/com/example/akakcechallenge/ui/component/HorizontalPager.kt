85c64ae501e9a6463217be6cc4b65beb938b0a10
Varol <>
2022-07-08 00:00:19 +0300

    Add horizontal scroll item card

--- app/src/main/java/com/example/akakcechallenge/ui/component/HorizontalPager.kt
+++ app/src/main/java/com/example/akakcechallenge/ui/component/HorizontalPager.kt
@@ -1,10 +1,6 @@
 package com.example.akakcechallenge.ui.component
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +12,7 @@ import com.google.accompanist.pager.rememberPagerState
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun HorizontalScroll(modifier: Modifier, listItems: List<Any>){
+fun HorizontalScroll(modifier: Modifier, listItems: List<Any>) {
     Column(modifier = modifier) {
         val pagerState = rememberPagerState()
         HorizontalPager(
@@ -27,14 +23,20 @@ fun HorizontalScroll(modifier: Modifier, listItems: List<Any>){
                 .weight(1f)
                 .fillMaxWidth(),
         ) { page ->
-            Text(text = listItems[page].toString())
+        HorizontalScrollItem(scrollItem = listItems[page])
         }
-
         HorizontalPagerIndicator(
             pagerState = pagerState,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(16.dp),
         )
+    }
+}
+
+@Composable
+fun HorizontalScrollItem(scrollItem: Any) {
+    Row(modifier = Modifier.wrapContentSize()) {
+        ItemWithDiscountCard(contentAlignment = Alignment.TopEnd)
     }
 }
