90e5c58f47f657832fc4ae2e77911ad72cea9239
Varol <>
2022-07-08 23:31:50 +0300

    Refactor UI

--- app/src/main/java/com/example/akakcechallenge/ui/component/ItemInfo.kt
+++ app/src/main/java/com/example/akakcechallenge/ui/component/ItemInfo.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.akakcechallenge.data.HorizontalProduct
@@ -19,9 +20,9 @@ fun ItemInfo(modifier: Modifier, item: HorizontalProduct) {
     Column(modifier = modifier) {
         Text(text = item.name.toString(), color = Color.Blue)
         Spacer(modifier = Modifier.width(4.dp))
-        Text(text = item.price.toString())
-        Text(text = item.countOfPrices.toString())
-        Text(text = item.followCount.toString())
+        Text(text = "${item.price.toString()} TL", fontWeight = FontWeight.ExtraBold)
+        Text(text = "${item.countOfPrices.toString()} satıcı >", fontWeight = FontWeight.Light)
+        Text(text = "${item.followCount.toString()} takip")
     }
 }
 
@@ -30,9 +31,9 @@ fun ItemInfo(modifier: Modifier, item: Product) {
     Column(modifier = modifier) {
         Text(text = item.name.toString(), color = Color.Blue)
         Spacer(modifier = Modifier.width(4.dp))
-        Text(text = item.price.toString())
-        Text(text = item.countOfPrices.toString())
-        Text(text = item.followCount.toString())
+        Text(text = "${item.price.toString()} TL", fontWeight = FontWeight.ExtraBold)
+        if (item.countOfPrices != null)Text(text = "${item.countOfPrices.toString()} satıcı >", fontWeight = FontWeight.Light)
+        if (item.followCount != null)Text(text = "${item.followCount.toString()} takip")
     }
 }
 
